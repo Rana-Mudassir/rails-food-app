31874c6151933692da800fe380a41abf3a0d10d3
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-07-25 22:11:37 +0430

    update

--- app/models/food.rb
+++ app/models/food.rb
@@ -7,5 +7,4 @@ class Food < ApplicationRecord
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true
-  validates :unit_quantity, presence: true
 end
