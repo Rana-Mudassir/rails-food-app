a785458422e712667c5b0937bfbcf589437bbf9d
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-07-26 17:25:23 +0530

    Fixing linters

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,42 +1,41 @@
 class InventoriesController < ApplicationController
-    def index
-        @inventories = Inventory.all
-      end
+  def index
+    @inventories = Inventory.all
+  end
 
-      def show
-        @inventory = Inventory.find(params[:id])
-        @food = Food.new
-      end
+  def show
+    @inventory = Inventory.find(params[:id])
+    @food = Food.new
+  end
 
-      def new
-        @inventory = Inventory.new
-      end
+  def new
+    @inventory = Inventory.new
+  end
 
-      def create
-        @inventory = Inventory.new(inventory_params)
-        respond_to do |format|
-          if @inventory.save
-            format.html { redirect_to inventory_path(@inventory), notice: 'Inventory was successfully created.' }
-            format.json { render :show, status: :created, location: @inventory }
-          else
-            format.html { render :new, status: :unprocessable_entity }
-            format.json { render json: @inventory.errors, status: :unprocessable_entity }
-          end
-        end
-      end
-
-
-      def destroy
-        @inventory.destroy
-        respond_to do |format|
-          format.html { redirect_to inventories_url, notice: 'Inventory was successfully destroyed.' }
-          format.json { head :no_content }
-        end       
-      end
-
-      private
-
-      def inventory_params
-        params.require(:inventory).permit(:name)
+  def create
+    @inventory = Inventory.new(inventory_params)
+    respond_to do |format|
+      if @inventory.save
+        format.html { redirect_to inventory_path(@inventory), notice: 'Inventory was successfully created.' }
+        format.json { render :show, status: :created, location: @inventory }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @inventory.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def destroy
+    @inventory.destroy
+    respond_to do |format|
+      format.html { redirect_to inventories_url, notice: 'Inventory was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name)
+  end
+end
