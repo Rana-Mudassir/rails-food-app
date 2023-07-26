238776b1bb2d87f01336346dfd709789cc0f086d
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-07-26 19:03:19 +0430

    some changes added to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    authenticated :user do
+      root to: 'pages#home', as: :authenticated_root
+    end
 
+    unauthenticated :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
   resources :foods
   resources :public_recipes, only: %i[index]
 
-  get 'users/index'
+  # get 'users/index'
 
   root "pages#home"
 end
