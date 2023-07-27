f4728a86a904730504a3c8ece00d87973886e3ea
Rana-Mudassir <mudassirnazird22@gmail.com>
2023-07-27 19:41:40 +0500

    Add modal for generating shopping list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,6 +14,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @user = @recipe.user
     @recipe_foods = @recipe.recipe_foods
+    @inventories = Inventory.all
   end
 
   def create
