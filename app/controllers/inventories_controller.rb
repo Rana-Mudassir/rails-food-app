5dbbe04fd198d146b1d121f3327ce89b936cf85b
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-07-29 09:14:57 +0430

    some changes added to the projects also test cases added

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -11,15 +11,20 @@ class InventoriesController < ApplicationController
 
   def destroy
     @inventory = Inventory.find(params[:id])
-
+  
+    # Delete the associated inventory_foods first
+    @inventory.inventory_foods.destroy_all
+  
     if @inventory.destroy
       flash[:notice] = 'Inventory deleted successfully!'
       redirect_to inventories_path
     else
-      flash[:alert] = 'Failed to delete the inventory. Please try again.'
-      redirect_to inventory_path(@inventory)
+      # Handle the case when the inventory cannot be deleted
+      flash[:error] = 'Error deleting inventory!'
+      redirect_to inventories_path
     end
   end
+  
 
   def new
     @new_inventory = Inventory.new
