b0ce4bd266a0312db6340ad840e157a908e0f152
Rana-Mudassir <mudassirnazird22@gmail.com>
2023-07-30 00:41:22 +0500

    Update Inventory design

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,6 +2,7 @@ class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods
   has_many :foods, through: :inventory_foods
-
+  
+  validates :description, presence: true
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
 end
